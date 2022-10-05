b4b62fa295b817e96b605f7368c654fdc760f1c5
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-10-05 12:03:43 -0700

    Created create method, working on rspec

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Workout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create email: 'joyce@test.com', name: 'Joyce', password: '123456', password_confirmation: '123456' }
+
+  it'should have a valid name' do
+    workout = user.workouts.create(
+      set_reps: '3x5',
+      weight: 155,
+    )
+
+    expect(workout.errors[:name]).to_not be_empty
+  end
+
+  # it'should have a valid set_reps' do
+  #   workout = user.workouts.create(
+  #     name: 'Joyce',
+  #     weight: 155
+  #   )
+
+  #   expect(workout.errors[:set_reps]).to_not be_empty
+  # end
+
+  # it'should have a valid weight' do
+  #   workout = user.workouts.create(
+  #     name: 'Luis',
+  #     set_reps: '3x10'
+  #   )
+
+  #   expect(workout.errors[:weight]).to_not be_empty
+  # end
+
 end
