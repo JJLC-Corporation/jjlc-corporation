f3898a897526d2e6c3cefac5f9ecd8b517305a44
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-10-04 20:52:58 -0700

    Changed button added navlink back to dashboard

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Button from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 
 const NotFound = () => {
   return (
     <>
     <h1>404 NOT FOUND</h1>
-    <div>
-        <Button color="danger">
-            Back to Home
-        </Button>
-</div>
+    <div className="notfound-button">
+        <NavLink to="/dashboard">
+            <Button color="danger">
+                Back to Home
+            </Button>
+        </NavLink>
+    </div>
     </>
   )
 }
