61afdeff71ad8e9d16dfb5d3997c45d7ac93595e
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-10-05 10:40:27 -0700

    working on seed file

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,6 @@
 class Workout < ApplicationRecord
+    def index
+        workouts = Workout.all
+        render json: workouts
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.where(email: 'test@company.com', name: 'Dave').first_or_create(password: 'password', password_confirmation: 'password')
+workout_seeds = [
+    {
+        name: 'bench press',
+        set_reps: '3x5',
+        weight: 155
+    },
+    {
+        name: 'squat',
+        set_reps: '3x5',
+        weight: 255
+    }
+]
+
+workout_seeds.each do |workout|
+    user.workouts.create(workout)
+    p "creating: #{workout}"
+  end
