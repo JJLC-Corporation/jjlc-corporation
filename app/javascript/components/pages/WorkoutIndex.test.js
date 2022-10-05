d7d55406c7facaa77d3d434aa850e694805ac652
jmagistrado <joycemagistrado@gmail.com>
2022-10-05 15:34:49 -0700

    fixed imports on test

--- app/javascript/components/pages/WorkoutIndex.test.js
+++ app/javascript/components/pages/WorkoutIndex.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react'
 import { BrowserRouter } from "react-router-dom"
-import WorkoutIndex from './workoutindex'
+import WorkoutIndex from './WorkoutIndex'
 
 
 describe('WorkoutIndex', () => {
