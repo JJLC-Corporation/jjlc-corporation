eafe7b0752cd1dfbf81251bf1647a144d34e0ce3
jmagistrado <joycemagistrado@gmail.com>
2022-10-11 00:58:05 -0700

    added contact info for styling

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -24,7 +24,7 @@ const Home = ({
     {/* second page */}
       <div className="welcome-bg">
         <h1 className="page-2">WHAT IS .LIFT()?</h1>
-          <p className="para-2">Can you recall a time where you felt lost and confused at the gym, wondering what you should do next? Yeah, us too... Too many times to admit. But here's the thing - we solved this problem for you! With .Lift(), you have the ability to plan your workout session before you head to the gym. Create your workout plan, see your session, edit your entry if you need to go higher in weight, or delete a workout if you don't feel like doing that exercise - it's all up to you.  
+          <p className="para-2">Can you recall a time where you felt lost and confused at the gym, wondering what you should do next? Yeah, us too... Too many times to admit. But here's the thing - no longer are the days you feel lost because we solved this issue! With .Lift(), you have the ability to plan your workout session before you head to the gym. Create your workout plan, see your session, edit your entry if you need to go higher in weight, or delete a workout if you don't feel like doing that exercise - it's all up to you. It's free to join us! Sign up now to start logging those sessions. 
           </p>
             <div className="button2-div">
               <Link to="/aboutus">
@@ -36,7 +36,14 @@ const Home = ({
                   <button className="button-right">JOIN US TODAY</button>
                 </a>
             </div>
-    </div>
+      </div>
+        <div className="page-3">
+          <h3 className="contact-us"><i><span className="connect">CONNECT</span> WITH US</i></h3>
+        <div className="email">
+          <h2 className="email-style"><i>JJLCCORP@GMAIL.COM</i></h2>
+
+        </div>
+      </div>
     </>
   )
 }
