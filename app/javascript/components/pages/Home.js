08ab60637e1d45d07e0a1e219dbefb8820d90175
jmagistrado <joycemagistrado@gmail.com>
2022-10-11 01:10:25 -0700

    updated css styling on email to see better

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -2,15 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = ({
-  logged_in,
-    current_user,
     new_user_route,
-    sign_in_route,
-    sign_out_route
+    sign_in_route
 }) => {
   return (
     <>
-    {/* first page */}
+    {/* First Section */}
     <div className="home-bg">
       <div className="home-title">
         <h1 className="title"><i>LET'S GO <span className="title-1">.LIFT( )</span></i></h1>
@@ -21,7 +18,7 @@ const Home = ({
           </div>
       </div>
     </div>
-    {/* second page */}
+    {/* Second Section */}
       <div className="welcome-bg">
         <h1 className="page-2">WHAT IS .LIFT()?</h1>
           <p className="para-2">Can you recall a time where you felt lost and confused at the gym, wondering what you should do next? Yeah, us too... Too many times to admit. But here's the thing - no longer are the days you feel lost because we solved this issue! With .Lift(), you have the ability to plan your workout session before you head to the gym. Create your workout plan, see your session, edit your entry if you need to go higher in weight, or delete a workout if you don't feel like doing that exercise - it's all up to you. It's free to join us! Sign up now to start logging those sessions. 
@@ -37,6 +34,7 @@ const Home = ({
                 </a>
             </div>
       </div>
+      {/* Third Section */}
         <div className="page-3">
           <h3 className="contact-us"><i><span className="connect">CONNECT</span> WITH US</i></h3>
         <div className="email">
