feebd8302c07b3268246d56e54cacc4c077031d0
jmagistrado <joycemagistrado@gmail.com>
2022-10-10 23:49:49 -0700

    unprotected home redesign

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Home = () => {
+const Home = ({
+  logged_in,
+    current_user,
+    new_user_route,
+    sign_in_route,
+    sign_out_route
+}) => {
   return (
-    <div>Home</div>
-    
+    <>
+    {/* first page */}
+    <div className="home-bg">
+      <div className="home-title">
+        <h1 className="title"><i>LET'S GO <span className="title-1">.LIFT( )</span></i></h1>
+          <div className="space-button">
+            <a href={sign_in_route}>
+              <button className="button">LOGIN</button>
+            </a>
+          </div>
+      </div>
+    </div>
+    {/* second page */}
+      <div className="welcome-bg">
+        <h1 className="page-2">WHAT IS .LIFT()?</h1>
+          <p className="para-2">Can you recall a time where you felt lost and confused at the gym, wondering what you should do next? Yeah, us too... Too many times to admit. But here's the thing - we solved this problem for you! With .Lift(), you have the ability to plan your workout session before you head to the gym. Create your workout plan, see your session, edit your entry if you need to go higher in weight, or delete a workout if you don't feel like doing that exercise - it's all up to you.  
+          </p>
+            <div className="button2-div">
+              <Link to="/aboutus">
+                <button className="button-left">
+                MEET THE TEAM
+                </button>
+              </Link>
+                <a href={new_user_route}>
+                  <button className="button-right">JOIN US TODAY</button>
+                </a>
+            </div>
+    </div>
+    </>
   )
 }
 
