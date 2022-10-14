b84e285ed2d4ba8d8d6a8598b3989d949299c327
jmagistrado <joycemagistrado@gmail.com>
2022-10-14 10:20:45 -0700

    updated indentation

--- app/javascript/components/pages/WorkoutNew.js
+++ app/javascript/components/pages/WorkoutNew.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Form, Row, Col, FormGroup, Label, Input, Button, Card } from "reactstrap";
+import {
+  Form,
+  Row,
+  Col,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Card,
+} from "reactstrap";
 
 const WorkoutNew = ({ createWorkout, logged_in, current_user }) => {
   const navigate = useNavigate();
@@ -9,7 +18,7 @@ const WorkoutNew = ({ createWorkout, logged_in, current_user }) => {
     name: "",
     set_reps: "",
     weight: "",
-    user_id: current_user.id
+    user_id: current_user.id,
   });
   const handleWorkouts = (e) => {
     setNewWorkout({ ...newWorkout, [e.target.name]: e.target.value });
@@ -18,61 +27,66 @@ const WorkoutNew = ({ createWorkout, logged_in, current_user }) => {
     createWorkout(newWorkout);
     navigate("/workoutindex");
   };
-console.log('workoutnew',current_user);
+  console.log("workoutnew", current_user);
   return (
     <>
-    <div className="new-bg"> 
-      <div className="components">
-      {logged_in && 
-        <div className="title-div">
-          <h1 className="new-title">ADD<span className="title-color"><i>YOUR</i></span></h1> 
-          <h1 className="new-title">WORKOUT</h1>
+      <div className="new-bg">
+        <div className="components">
+          {logged_in && (
+            <div className="title-div">
+              <h1 className="new-title">
+                ADD
+                <span className="title-color">
+                  <i>YOUR</i>
+                </span>
+              </h1>
+              <h1 className="new-title">WORKOUT</h1>
+            </div>
+          )}
+          {logged_in && (
+            <div className="form">
+              <Card style={{ height: "27rem", width: "25rem" }}>
+                <Form>
+                  <FormGroup className="new-form-group">
+                    <Label for="name">Workout Name</Label>
+                    <Input
+                      name="name"
+                      placeholder="Enter a name for your workout"
+                      type="text"
+                      onChange={handleWorkouts}
+                      value={newWorkout.name}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="set_reps">Sets x Reps</Label>
+                    <Input
+                      name="set_reps"
+                      placeholder="Enter your Sets and Reps"
+                      type="text"
+                      onChange={handleWorkouts}
+                      value={newWorkout.set_reps}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="weight">Weight</Label>
+                    <Input
+                      name="weight"
+                      placeholder="Enter the weight"
+                      type="text"
+                      onChange={handleWorkouts}
+                      value={newWorkout.weight}
+                    />
+                  </FormGroup>
+                  <div className="new-button">
+                    <Button onClick={handleSubmit} color="danger" name="submit">
+                      Submit Workout
+                    </Button>
+                  </div>
+                </Form>
+              </Card>
+            </div>
+          )}
         </div>
-      }
-      {logged_in && (
-        <div className="form">
-        <Card style={{height:'27rem', width:'25rem'}}>
-        <Form>
-              <FormGroup className="new-form-group">
-                <Label for="name">Workout Name</Label>
-                <Input
-                  name="name"
-                  placeholder="Enter a name for your workout"
-                  type="text"
-                  onChange={handleWorkouts}
-                  value={newWorkout.name}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="set_reps">Sets x Reps</Label>
-                <Input
-                  name="set_reps"
-                  placeholder="Enter your Sets and Reps"
-                  type="text"
-                  onChange={handleWorkouts}
-                  value={newWorkout.set_reps}
-                />
-              </FormGroup>
-          <FormGroup>
-            <Label for="weight">Weight</Label>
-            <Input
-              name="weight"
-              placeholder="Enter the weight"
-              type="text"
-              onChange={handleWorkouts}
-              value={newWorkout.weight}
-            />
-          </FormGroup>
-          <div className="new-button">
-          <Button onClick={handleSubmit} color="danger" name="submit">
-            Submit Workout
-          </Button>
-          </div>
-        </Form>
-        </Card>
-        </div>
-      )}
-      </div>
       </div>
       {!logged_in && <h1>Please sign in</h1>}
     </>
--- app/javascript/components/pages/WorkoutShow.js
+++ app/javascript/components/pages/WorkoutShow.js
@@ -8,35 +8,49 @@ const WorkoutShow = ({ workouts, deleteWorkout }) => {
   const workout = workouts?.find((workout) => workout.id === +id);
 
   const handleDelete = () => {
-    deleteWorkout(id)
-    console.log(workout)
-  }
+    deleteWorkout(id);
+    console.log(workout);
+  };
 
   return (
     <>
-    <div className="show-bg">
-    <h1 className="show-title">
-      <i><span className="my-title">MY</span> WORKOUT</i>
-    </h1>
-      <div className="WorkoutShow">
-        <Card style={{ width: '25rem', height: '25rem' }} >
-          <CardBody>
-            <CardTitle tag="h5" className="card-t">{workout.name}</CardTitle>
-            <CardText className="show-info"><b>SetsxReps:</b> {workout.set_reps}</CardText>
-            <CardText className="show-info"><b>Weight:</b> {workout.weight}lbs</CardText>
+      <div className="show-bg">
+        <h1 className="show-title">
+          <i>
+            <span className="my-title">MY</span> WORKOUT
+          </i>
+        </h1>
+        <div className="WorkoutShow">
+          <Card style={{ width: "25rem", height: "25rem" }}>
+            <CardBody>
+              <CardTitle tag="h5" className="card-t">
+                {workout.name}
+              </CardTitle>
+              <CardText className="show-info">
+                <b>SetsxReps:</b> {workout.set_reps}
+              </CardText>
+              <CardText className="show-info">
+                <b>Weight:</b> {workout.weight}lbs
+              </CardText>
               <div className="b-style">
                 <div className="edit-button">
-                <NavLink to={`/workoutedit/${workout.id}`} >
-                  <Button color="danger">Edit Workout</Button>
-                </NavLink>
+                  <NavLink to={`/workoutedit/${workout.id}`}>
+                    <Button color="danger">Edit Workout</Button>
+                  </NavLink>
                 </div>
-                <DeleteConfirmation  color="danger" handleDelete={handleDelete} />
+                <DeleteConfirmation
+                  color="danger"
+                  handleDelete={handleDelete}
+                />
               </div>
-          </CardBody>
-        </Card>
-      </div>
-      <div className="div-padding">
-        <img src="https://i.postimg.cc/L6vcPfgc/logo-1.png" className="about-logo"></img>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="div-padding">
+          <img
+            src="https://i.postimg.cc/L6vcPfgc/logo-1.png"
+            className="about-logo"
+          ></img>
         </div>
       </div>
     </>
