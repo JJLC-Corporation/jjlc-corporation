14c6d9256920b73fe1569225e70d7391bd15492f
jmagistrado <joycemagistrado@gmail.com>
2022-10-16 10:12:51 -0700

    Was able to get navbar working

--- app/javascript/components/components/Navigation.js
+++ app/javascript/components/components/Navigation.js
@@ -1,98 +1,85 @@
-import React from "react"
-import { Nav, NavItem, Button} from "reactstrap"
-import { NavLink } from "react-router-dom"
-
-
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 
 const Navigation = ({
-    logged_in,
-    current_user,
-    new_user_route,
-    sign_in_route,
-    sign_out_route
+  logged_in,
+  current_user,
+  new_user_route,
+  sign_in_route,
+  sign_out_route,
 }) => {
-    return (
-    <>
-        <div className="logged-in-navbar">
-            <Nav>
-            {logged_in && (
-                <NavItem>
-                    <NavLink to="/dashboard" className="nav-link">
-                        <Button>Home</Button>
-                    </NavLink>
-                </NavItem>
-                )}
-            {logged_in && (
-                <NavItem>
-                <a href={sign_out_route} className="nav-link">
-                    <Button>Sign Out</Button>
-                </a>
-                </NavItem>
-            )}
-            {logged_in && (
-                <NavItem>
-                    <NavLink to="/workoutindex" className="nav-link">
-                        <Button>All Workouts</Button>
-                    </NavLink>
-                </NavItem>
-            )}
-            {logged_in && (
-                <NavItem>
-                    <NavLink to="/workoutnew" className="nav-link">
-                        <Button>Add Workout</Button>
-                    </NavLink>
-                </NavItem>
-            )}
-            {logged_in && (
-                <NavItem>
-                    <NavLink to="/resources" className="nav-link">
-                        <Button>Resources</Button>
-                    </NavLink>
-                </NavItem>
-            )}
-            </Nav>
-        </div>
-        <div className="logged-out-navbar">
-            <Nav>
-            {!logged_in && (
-                <NavItem>
-                    <NavLink to="/" className="nav-link">
-                        <Button>Home</Button>
-                    </NavLink>
-                </NavItem>
-                )}
-            {!logged_in && (
-                <NavItem>
-                <a href={sign_in_route} className="nav-link">
-                    <Button>Sign In</Button>
-                </a>
-                </NavItem>
-            )}
-            {!logged_in && (
-                <NavItem>
-                <a href={new_user_route} className="nav-link">
-                    <Button>Sign Up</Button>
-                </a>
-                </NavItem>
-            )}
-            {!logged_in && (
-                <NavItem>
-                    <NavLink to="/aboutus" className="nav-link">
-                        <Button>About Us</Button>
-                    </NavLink>
-                </NavItem>
-            )}
-            {!logged_in && (
-                <NavItem>
-                    <NavLink to="/resources" className="nav-link">
-                        <Button>Resources</Button>
-                    </NavLink>
-                </NavItem>
-            )}
-            </Nav>
-        </div>
-    </>
-    )
-}
+  const [collapsed, setCollapsed] = useState(true);
 
-export default Navigation
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <>
+      {!logged_in && (
+        <div>
+          <Navbar color="faded" light>
+            <NavbarBrand href="/" className="me-auto">
+              <span className="lift-title">
+                <i>.LIFT</i>
+              </span>
+            </NavbarBrand>
+            <NavbarToggler onClick={toggleNavbar} className="me-2" />
+            <Collapse isOpen={!collapsed} navbar>
+              <Nav navbar>
+                <NavLink href={sign_in_route} className="nav-link">
+                  Login
+                </NavLink>
+                <NavLink href={new_user_route} className="nav-link">
+                  Sign Up
+                </NavLink>
+                <NavLink href="/resources" className="nav-link">
+                  Workout Resources
+                </NavLink>
+                <NavLink href="/aboutus" className="nav-link">
+                  Meet The Team
+                </NavLink>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      )}
+      {logged_in && (
+        <div>
+          <Navbar color="faded" light>
+            <NavbarBrand href="/dashboard" className="me-auto">
+              <span className="lift-title">
+                <i>.LIFT</i>
+              </span>
+            </NavbarBrand>
+            <NavbarToggler onClick={toggleNavbar} className="me-2" />
+            <Collapse isOpen={!collapsed} navbar>
+              <Nav navbar>
+                <NavLink href="/workoutindex" className="nav-link">
+                  Workout Plan
+                </NavLink>
+                <NavLink href="/workoutnew" className="nav-link">
+                  Add a New Workout
+                </NavLink>
+                <NavLink href="/resources" className="nav-link">
+                  Workout Resources
+                </NavLink>
+                <NavLink href={sign_out_route} className="nav-link">
+                  Log Out
+                </NavLink>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Navigation;
