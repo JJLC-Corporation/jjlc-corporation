19df37e688ee42376f0bfc6a6065bcf9b5b9ea3f
luislaurel <luislaurel133@gmail.com>
2022-10-16 19:17:12 -0600

    Font size 100

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -5,14 +5,14 @@ import { NavLink } from "react-router-dom";
 const NotFound = () => {
   return (
     <>
-    <div className="notfound">
-      <h1>404 NOT FOUND</h1>
-      <div className="notfound-button">
-        <NavLink to="/">
-          <Button color="danger">Back to Home</Button>
-        </NavLink>
+      <div className="notfound">
+        <h1 style={{ fontSize: 100 }}>404 NOT FOUND</h1>
+        <div className="notfound-button">
+          <NavLink to="/">
+            <Button color="danger">Back to Home</Button>
+          </NavLink>
+        </div>
       </div>
-    </div>
     </>
   );
 };
